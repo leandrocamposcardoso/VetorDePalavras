36a1e9b6edb208e3a4c206b1a1d07ec851655e79
Leandro Cardoso <leandrocamposcardsoso0@gmail.com>
2020-09-12 17:47:45 -0300

    Initial project base

---
+++ api/nlp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from django.contrib import admin
+from django.conf import settings
+
+api_base = settings.URL_BASE
+
+urlpatterns = [
+    path(f"{api_base}admin/", admin.site.urls),
+    # path(f'{api_base}words_vector/', include('words_vector.urls')),
+]
---
+++ api/words_vector/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WordsVectorConfig(AppConfig):
+    name = "words_vector"
